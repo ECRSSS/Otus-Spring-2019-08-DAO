5dd144405493f8964ee2e9739cb986254947f73e
nik <nikng2016@yandex.ru>
2020-01-17 23:53:40 +0300

    aspect commented

--- src/main/java/nnglebanov/daoexample/aspects/EntityDetachAspect.java
+++ src/main/java/nnglebanov/daoexample/aspects/EntityDetachAspect.java
@@ -1,16 +1,13 @@
 package nnglebanov.daoexample.aspects;
 
 import org.aspectj.lang.annotation.Aspect;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
-
-import javax.persistence.EntityManager;
 
 @Aspect
 @Component
 public class EntityDetachAspect {
-    @Autowired
-    EntityManager em;
+   // @Autowired
+   // EntityManager em;
 /*
     @AfterReturning(
             pointcut = "execution(* nnglebanov.daoexample.repositories.impl.*.*(..))",
