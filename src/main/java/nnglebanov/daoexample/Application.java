d6b20aa68e900d23baaf2aa9f32a856fc2f07b9f
ecrsss <n.n.glebanov@gmail.com>
2020-02-10 01:39:09 +0300

    first controller

--- src/main/java/nnglebanov/daoexample/Application.java
+++ src/main/java/nnglebanov/daoexample/Application.java
@@ -2,6 +2,7 @@ package nnglebanov.daoexample;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.PropertySource;
 
 
 @SpringBootApplication
--- src/main/java/nnglebanov/daoexample/rest/BookController.java
+++ src/main/java/nnglebanov/daoexample/rest/BookController.java
@@ -3,13 +3,23 @@ package nnglebanov.daoexample.rest;
 import lombok.NonNull;
 import lombok.RequiredArgsConstructor;
 import nnglebanov.daoexample.repositories.BookRepository;
+import nnglebanov.daoexample.rest.dto.BookDto;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+import java.util.stream.Collectors;
 
 @RequiredArgsConstructor
 @RestController
 public class BookController {
     @NonNull
     private final BookRepository bookRepository;
+
+    @GetMapping("/api/books")
+    public List<BookDto> getAllBooks(){
+        return bookRepository.findAll().stream().map(BookDto::toDto).collect(Collectors.toList());
+    }
 
 
 }
